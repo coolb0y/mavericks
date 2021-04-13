3c17e69cdf325e5053c4841701a9deb07429a04e
coolb0y <ankurnarayan2016@gmail.com>
2021-04-13 15:26:25 +0530

    Mavericks Project

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = [];
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.render("mavericks",{newListItem: items});
+})
+
+app.post("/",function(req,res){
+var item = req.body.newItem;
+items.push(item);
+res.redirect("/");
+})
+
+app.get("/delete",function(req,res){
+  items.pop();
+  res.redirect("/");
+})
+
+app.listen( process.env.port ||3000, function() {
+  console.log("Server is running on port 3000");
+})
---
+++ index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+
+          window.location.href="/delete";
+
+  }
+}
+
+//myFunction();
